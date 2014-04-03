f3d98afc4fcd8d76c572dee51ae19a4ca9cb22f8
Enrique Tron <enrique.tron@gmail.com>
2014-04-03 13:24:05 -0400

    regrese a foundation 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require foundation
+//= require custom
+//= require vendor/jquery.prettyPhoto
 //= require_tree .
 $(function() {
   $(document).foundation();
