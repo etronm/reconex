cfdb5782535e7d3595de88a5d722c5cd93e70333
Enrique Tron <enrique.tron@gmail.com>
2014-04-05 13:59:04 -0400

    dandole forma 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Reconectese::Application.routes.draw do
   resources :visitors, only: [:new, :create]
   resources :contacts, only: [:new, :create]
-  root to: 'visitors#new'
+  #root to: 'visitors#new'
+  root to: redirect('/pages/intro.html')
+
 end
