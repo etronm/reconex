abfcd2b5a1988034e39ff75daf277e40a0b278a5
Enrique Tron <enrique.tron@gmail.com>
2014-04-08 10:54:45 -0400

    con la tabla de usuarios creada

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Reconectese::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
   resources :visitors, only: [:new, :create]
   resources :contacts, only: [:new, :create]
   resources :users, only: [:new, :create]
