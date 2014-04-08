d13409a586587b159ba6c92fdb448cf4a1a6420b
Enrique Tron <enrique.tron@gmail.com>
2014-04-08 12:02:19 -0400

    con pruebas integrales

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Reconectese::Application.routes.draw do
+
   get "static_pages/home"
   get "static_pages/help"
+
   resources :visitors, only: [:new, :create]
   resources :contacts, only: [:new, :create]
   resources :users, only: [:new, :create]
