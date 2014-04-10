1b8abe2c18347722952baf7785ca5c67bd51eb60
Enrique Tron <enrique.tron@gmail.com>
2014-04-10 12:03:56 -0400

    faltantes

---
+++ db/migrate/20140409154241_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :name
+      t.string :phone
+      t.string :password
+      t.integer :user_type
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "Root page" do
+
+    it "should have the content 'Aquitoy'" do
+      visit '/pages/intro.html'
+      expect(page).to have_content('Aquitoy')
+    end
+  end
+
+end
