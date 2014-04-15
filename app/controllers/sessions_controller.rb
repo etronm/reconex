877a87baabebb2b29a0d6345511847d08cd2384f
Enrique Tron <enrique.tron@gmail.com>
2014-04-14 20:16:05 -0400

    restaurando del desmadre, con login ok

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+
+    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #algo
+      flash[:success] = 'Felicidades!'
+    else
+      flash[:error] = 'Invalid email/password combination'
+      render new
+    end
   end
 
   def destroy
