129d33e4341961d7e3583cd87af87add5cc72f86
Enrique Tron <enrique.tron@gmail.com>
2014-04-15 09:41:16 -0400

    con el mapa funcionando

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,33 +1,14 @@
 var latitude = "18.393186";
 var longitude = "-66.155249";
 var details = "Beetle Inc - Bayamon, Puerto Rico";
+var handler = Gmaps.build('Google');
 
-function LoadGmaps() {
-    var myLatlng = new google.maps.LatLng(latitude, longitude);
-    var myOptions = {
-        zoom: 12,
-        scrollwheel: true,
-        center: myLatlng,
-        navigationControl: true,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-
-    var map = new google.maps.Map(document.getElementById("googlemaps"), myOptions);
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        icon: 'map_icon.png'});
-
-    var infowindow = new google.maps.InfoWindow({content: details});
-
-google.maps.event.addListener(marker, "click", function () {
-    infowindow.open(map, marker);
-
+handler.buildMap({ internal: {id: 'etm_map'}}, function(){
+    var marker = handler.addMarker({
+            lat: latitude,
+            lng: longitude,
+            infowindow: details
+        });
+    handler.map.centerOn(marker);
+    handler.getMap().setZoom(12);
 });
-
-}
-
-$(window).load(function () {
-LoadGmaps();
-});
-
