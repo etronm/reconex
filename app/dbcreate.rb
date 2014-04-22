986bc62c0a5596c5dbed2d950c2411f46f3567e4
enrique tron <enrique.tron@gmail.com>
2014-04-22 14:33:53 -0400

    BD propuesta
    Para crear la base de datos original

--- app/dbcreate.rb
+++ app/dbcreate.rb
@@ -2,3 +2,26 @@
 #
 rails generate model User email:string name:string phone:string password:string user_type:integer
 # user = User.new(name: 'enrique', email: 'et@gg.com')
+
+--- validar si user tiene un numero int para utilizarlo como key
+
+Section id:int description:string
+
+Article id:int title:string description:string status:int author_id:int date_published:date banner_image:string 
+	comments_enabled:boolean views:int featured:boolean 
+	
+ArticleSection id:int article_id:int section_id:int description:string status:int
+ArticleCategory category_id:int article_id:int 
+ArticleTag id:int article_id:int tag:string tag_clean;string
+ArticleRelated article_id:int article_related_id:int
+
+ArticlePhotos article_id:int section_id:int photo_path:string album_url:string
+
+Comment id:int article_id:int section_id:int user_id:int comment:string status:int is_reply_to_id:int date_posted:date
+CommentUser id:int user_id:int name:string email:string website:string
+
+Attribute id:int name:string
+Category id:int name:string name_clean:string ^^description:string ^^information:string status:int
+CategoryAttribute id:int category_id:int attribute_id:int 
+
+
