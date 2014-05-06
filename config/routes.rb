8f966ad9ca39e9d121cec7506aa94b9d162c826c
Enrique Tron <enrique.tron@gmail.com>
2014-05-05 20:55:58 -0400

    rutas en url sin parametros

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Reconectese::Application.routes.draw do
 
-  get "static_pages/home"
-  get "static_pages/help"
+  scope "(:locale)", locale: /en|es/ do
 
-  resources :visitors, only: [:new, :create]
-  resources :contacts, only: [:new, :create]
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :articles, only: [:create, :destroy]
-  resources :sections
+    resources :visitors, only: [:new, :create]
+    resources :contacts, only: [:new, :create]
+    resources :users
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :articles, only: [:create, :destroy]
+    resources :sections
 
-  root to: redirect('/pages/intro.html')
+    root to: redirect('/pages/intro.html')
 
-  match '/signup',  to: 'users#new', via: 'get'
-  match '/signin',  to: 'sessions#new', via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
+    match '/signup', to: 'users#new', via: 'get'
+    match '/signin', to: 'sessions#new', via: 'get'
+    match '/signout', to: 'sessions#destroy', via: 'delete'
+  end
 
 end
