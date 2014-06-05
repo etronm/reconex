371a76349d69e6a90098d4f40cb471e02ca02d93
Enrique Tron <enrique.tron@gmail.com>
2014-06-05 17:57:30 -0400

    pasar a env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :aws_access_key_id      => 'AKIAJ2YIE4L7T2K6AAEA',                        # required
-      :aws_secret_access_key  => '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',                        # required
+      :aws_access_key_id      => ENV["AWS_KEY"],                        # required
+      :aws_secret_access_key  => ENV["AWS_SKEY"],                        # required
       :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
