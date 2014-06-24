b4aa9432a4e47305eb4e8b8132f146f08d17c1d8
Enrique Tron <enrique.tron@gmail.com>
2014-06-24 19:00:50 -0400

    editando el articulo primario

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,9 @@ module ApplicationHelper
     link_to "<i class='icon-trash icon-border red-link icon-large #{align} title='Borrar'></i>".html_safe, object_to_delete,
             method: :delete, data: {confirm: "Seguro?"}
   end
+  def create_edit_icon(path_to_edit, align='')
+    link_to "<i class='icon-edit icon-border blue-link icon-large #{align} title='Editar'></i>".html_safe, path_to_edit
+  end
 
   def create_big_link(icon, title, path)
     link_to "<i class='#{icon} icon-large'>  #{title}</i>".html_safe, path, class: "right dark-link"
