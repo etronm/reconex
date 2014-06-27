fd53da4f460240ee1928a53461cc1719324441aa
Enrique Tron <enrique.tron@gmail.com>
2014-06-27 08:07:24 -0400

    Create section on the fly....

--- app/controllers/article_contents_controller.rb
+++ app/controllers/article_contents_controller.rb
@@ -8,17 +8,19 @@ class ArticleContentsController< ApplicationController
 
   def index
     @article = Article.find(params[:article_id])
-
   end
 
   def create
+    new_section_name = params[:article_content][:new_section_name]
+    @section = Section.where(:name => new_section_name, :description => new_section_name, status: 0).first_or_create
     @article = Article.find(params[:article_id])
     @article_content = ArticleContent.new(
         article: @article,
-        section_id: params[:article_content][:section_id],
+        section: @section,
         description: params[:article_content][:description],
         status:params[:article_content][:status],
-        display_order:params[:article_content][:display_order])
+        display_order: params[:article_content][:display_order])
+
     if @article_content.save
       flash[:success] = t(:section_create_success)
       redirect_to article_article_contents_path @article
@@ -30,7 +32,8 @@ class ArticleContentsController< ApplicationController
 
   private
   def request_params
-    params.require(:article_content).permit(:description, :status, :display_order, :section_id, :article_id)
+    params.require(:article_content).permit(:description, :status, :display_order,
+                                            :section_id, :article_id, :new_section_name)
   end
 
 
